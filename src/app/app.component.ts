3956730d4f2f650d63af5f6117b266ac57ae6df6
Andres Fabian Patiño Bermudez <97bermudez.andres@gmail.com>
2023-09-06 16:19:02 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
-
+import {
+  ActionPerformed,
+  PushNotifications,
+  PushNotificationSchema,
+  Token,
+} from '@capacitor/push-notifications';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.pushSetup();
+    });
+  }
+
+  pushSetup() {
+    PushNotifications.requestPermissions().then((res: any) => {
+      if (res.receive === 'granted') {
+        console.log('Permiso de notificaciones habilitado');
+      } else {
+        console.log('Permiso de notificaciones deshabilitado');
+      }
+    });
+
+    PushNotifications.addListener('registration', (token: Token) => {
+      console.log('Token de registro:', token);
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        console.log('Notificación recibida:', notification);
+      }
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        console.log('Acción de notificación realizada:', notification);
+      }
+    );
+  }
 }
